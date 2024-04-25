eded4fb4ab51755fa81f154ca012e5d7672fd4ee
Thitiphum Chaikarnjanakit <thitiphum.dev@gmail.com>
2024-04-25 07:01:08 +0700

    Add CSV tax calculation functionality to services and controllers
    This commit adds two key functions to support tax calculations from CSV data:
    
    1. `CalculateTaxFromCSV` in the tax service:
       - Calculates taxes and refunds from CSV records.
       - Leverages existing tax brackets and deductions.
    
    2. `CalculateCSVTax` in the tax controller:
       - Endpoint for CSV tax calculation submissions.
       - Parses CSV data and outputs structured JSON tax calculations.

--- applications/services/tax_service.go
+++ applications/services/tax_service.go
@@ -7,4 +7,5 @@ import (
 type TaxServiceInterface interface {
 	CalculateTax(income float64, wht float64, allowances []schemas.Allowance) (float64, float64, error)
 	CalculateDetailedTax(income float64, wht float64, allowances []schemas.Allowance) ([]schemas.TaxLevel, float64, float64, error)
+	CalculateTaxFromCSV(records []schemas.CSVObjectFormat) (schemas.CSVResponse, error)
 }
--- interfaces/endpoints/controllers/tax_controller.go
+++ interfaces/endpoints/controllers/tax_controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"encoding/csv"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/thitiphum-bluesage/assessment-tax/applications/services"
@@ -72,6 +76,71 @@ func (tc *TaxController) CalculateDetailedTax(c echo.Context) error {
 	response := schemas.DetailedTaxCalculationResponse{
 		Tax:      netTax,
 		TaxLevel: taxLevel,
+	}
+	return c.JSON(http.StatusOK, response)
+}
+
+func (tc *TaxController) CalculateCSVTax(c echo.Context) error {
+	fileHeader, err := c.FormFile("taxFile")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Failed to get the file")
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to open the file")
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	headers, err := csvReader.Read()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to read headers from CSV file")
+	}
+
+	columnIndex := make(map[string]int)
+	for i, header := range headers {
+		columnIndex[strings.ToLower(header)] = i
+	}
+
+	// {
+	// 	"totalincome": 0,
+	// 	"wht": 1,
+	// 	"donation": 2,
+	// 	"kreceipt": 3
+	// }
+
+	var taxRecords []schemas.CSVObjectFormat
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to read record from CSV file")
+		}
+
+		var taxRecord schemas.CSVObjectFormat
+		if index, ok := columnIndex["totalincome"]; ok {
+			taxRecord.TotalIncome, _ = strconv.ParseFloat(record[index], 64)
+		}
+		if index, ok := columnIndex["wht"]; ok {
+			taxRecord.WHT, _ = strconv.ParseFloat(record[index], 64)
+		}
+		if index, ok := columnIndex["donation"]; ok {
+			taxRecord.Donation, _ = strconv.ParseFloat(record[index], 64)
+		}
+		// k-receipt will = 0 if not provided
+		if index, ok := columnIndex["k-receipt"]; ok {
+			taxRecord.KReceipt, _ = strconv.ParseFloat(record[index], 64)
+		}
+
+		taxRecords = append(taxRecords, taxRecord)
+	}
+
+	response, err := tc.taxService.CalculateTaxFromCSV(taxRecords)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, response)
 }
